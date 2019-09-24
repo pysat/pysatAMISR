d7eb8a670682886711c07217be2c3b2b6ee30f4a
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2019-09-24 11:06:00 -0400

    ENH: created __init__.py
    Added basic import support.

---
+++ pysatAMISR/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-.
+"""Module for loading AMISR data into pysat
+
+Author
+------
+Angeline G. Burrell, NRL, September 2019
+
+Modules
+-------
+methods
+isr_pf
+isr_risrn
+
+"""
+
+from __future__ import absolute_import
+
+from . import methods
+from . import isr_pf
+
+__version__ = '1.0.0'
+
+
